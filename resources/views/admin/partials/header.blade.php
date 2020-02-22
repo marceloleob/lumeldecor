811c4983abd6e61158b3605d83a6dd21a4f83de8
Marcelo Leopold <marceloleob@gmail.com>
2020-02-21 20:16:08 -0500

    Testando menu deslogado

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -13,16 +13,7 @@
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav ml-auto">
-                                <li class="nav-item"> <a class="nav-link" href="">Início</a></li>
-                                <li class="nav-item"> <a class="nav-link" href="">Anunciar meu Imóvel</a></li>
-                                <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="blog-grid-classic.html" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Serviços</a>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="">Gestão Imobiliária</a></li>
-                                        <li><a class="dropdown-item" href="">Consultoria Jurídica</a></li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item"> <a class="nav-link" href="{!! route('about') !!}">Sobre Shekinah</a></li>
-                                <li class="nav-item"> <a class="nav-link" href="{!! route('contact') !!}">Contato</a></li>
+                                <li class="nav-item"> <a class="nav-link" href="#">MENU</a></li>
                             </ul>
                         </div>
                     </nav>
