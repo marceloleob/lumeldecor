80d4d3a66c17fa805e94200bebb9bd261201020d
“Marcelo <marceloleob@gmail.com>
2020-06-02 16:46:11 -0400

    Teste

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -19,7 +19,7 @@ class ImageService
 	{
 		// cria um nome para a imagem
 		$fileName = date('Y-m-d') . '-' . uniqid() . '.' . $picture->extension();
-dd($fileName);
+dump($fileName);
 		// salva a imagem na pasta Regular
 		$fullName = $picture->storeAS(config('constants.PICTURES_PATHS.REGULAR'), $fileName, 'public');
 dd($fullName);
