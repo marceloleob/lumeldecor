b7934468282baa0ac84056a0fb395bcc43f1d573
“Marcelo <marceloleob@gmail.com>
2020-06-02 16:45:44 -0400

    Teste

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -19,8 +19,10 @@ class ImageService
 	{
 		// cria um nome para a imagem
 		$fileName = date('Y-m-d') . '-' . uniqid() . '.' . $picture->extension();
+dd($fileName);
 		// salva a imagem na pasta Regular
 		$fullName = $picture->storeAS(config('constants.PICTURES_PATHS.REGULAR'), $fileName, 'public');
+dd($fullName);
 		// redimensiona as imagens (bigger, regular e thumbnail)
 		self::resize($fullName, $fileName);
 
