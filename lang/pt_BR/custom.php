a4c4d9e81619450db3b13701c70a28982376d8ba
Marcelo Leopold <marceloleob@gmail.com>
2026-01-15 21:10:13 -0500

    Add lib for translate Laravel messages to Portuguese

---
+++ lang/pt_BR/custom.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'attributes' => [
+        'name' => 'nome',
+        'email' => 'e-mail',
+        'password' => 'senha',
+        'password_confirmation' => 'confirmação de senha',
+        'phone' => 'telefone',
+        'cpf' => 'CPF',
+        'cnpj' => 'CNPJ',
+        'zip_code' => 'CEP',
+        'street' => 'rua',
+        'number' => 'número',
+        'neighborhood' => 'bairro',
+        'city' => 'cidade',
+        'state' => 'estado',
+        'price' => 'preço',
+        'stock_quantity' => 'quantidade em estoque',
+    ],
+];
